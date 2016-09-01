1917e6e3798aa17b7d5deef85ff632b63b2ae14d
Jan Klepek <jan@klepek.cz>
2016-09-01 19:36:39 +0200

    Initial release

---
+++ digmia-logvol-backup/en.dict.digmia-logvol-backup.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+'LogicalVolume:basic' => 'Basic information',
+'LogicalVolume:backup' => 'Backup information',
+'Class:LogicalVolume/Attribute:volume_type'=> 'Volume Type',
+'Class:LogicalVolume/Attribute:backup_hourly'=> 'Hourly',
+'Class:LogicalVolume/Attribute:backup_daily'=> 'Daily',
+'Class:LogicalVolume/Attribute:backup_weekly'=> 'Weekly',
+'Class:LogicalVolume/Attribute:backup_monthly'=> 'Monthly',
+	// Dictionary entries go here
+));
+?>
---
+++ digmia-mail-objects/en.dict.digmia-mail-objects.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2016 Jan Klepek
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+        'Class:Software/Attribute:type/Value:MailServer' => 'Mail server',
+        'Class:Software/Attribute:type/Value:MailServer+' => 'Mail servers',
+	'Class:MailDomain' => 'Mail domain',
+	'Class:MailDomain+' => 'Mail domains',
+        'Class:MailServer' => 'Mail server',
+        'Class:MailServer+' => 'Mail servers',
+	'Class:MailServer/Attribute:maildomain_list' => 'Mail domain list',
+        'Class:MailServer/Attribute:maildomain_list+' => 'Mail domain list',
+	'Class:MailDomain/Attribute:usercount' => 'Number of users',
+	'Class:MailDomain/Attribute:usercount+' => 'Number of users',
+        'Class:MailDomain/Attribute:size' => 'Size in MB',
+        'Class:MailDomain/Attribute:size+' => 'Size in MB',
+        'Class:MailDomain/Attribute:status' => 'Status of mail domain',
+        'Class:MailDomain/Attribute:status+' => 'Status of mail domain',
+        'Class:MailDomain/Attribute:mailserver_id' => 'Mailserver name',
+        'Class:MailDomain/Attribute:mailserver_id+' => 'Mailserver name',
+
+));
+?>
